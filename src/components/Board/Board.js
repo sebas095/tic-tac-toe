48b4debae2aa6c17ef6a016c3f4cb7f9064efb6c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-03-20 22:46:33 -0500

    [ADD] winner, draw and reverse funtions added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,35 +2,33 @@ import React, { Component } from "react";
 import Square from "../Square/Square";
 import "./Board.css";
 
+const BoardRow = ({ children }) => <div className="board-row">{children}</div>;
+
 export class Board extends Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+  createBoard() {
+    const { isWinner } = this.props;
+
+    let board = [];
+    for (let row = 0; row < 3; row++) {
+      let rows = [];
+      for (let col = 0; col < 3; col++) {
+        const index = row * 3 + col;
+        rows.push(
+          <Square
+            key={row * 3 + col}
+            value={this.props.squares[index]}
+            onClick={() => this.props.onClick(index)}
+            className={isWinner.includes(index) ? " isWinner" : ""}
+          />
+        );
+      }
+      board.push(<BoardRow key={"row-" + row}>{rows}</BoardRow>);
+    }
+
+    return board;
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.createBoard()}</div>;
   }
 }
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import { Board } from "../Board/Board";
-import { calculateWinner } from "../../utils";
+import Switch from "react-switch";
+import {
+  calculateWinner,
+  indexToRowCol,
+  calculateWinnerIndex,
+  isDraw
+} from "../../utils";
 import "./Game.css";
 
 export class Game extends Component {
   state = {
     history: [
       {
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        prevMove: null
       }
     ],
     stepNumber: 0,
-    xIsnext: true
+    xIsnext: true,
+    isActive: false,
+    checked: false
   };
 
-  handleclick(i) {
+  handleClick(i) {
     const { xIsNext, stepNumber } = this.state;
     const history = this.state.history.slice(0, stepNumber + 1);
     const current = history[history.length - 1];
@@ -24,38 +33,56 @@ export class Game extends Component {
 
     squares[i] = xIsNext ? "X" : "O";
     this.setState({
-      history: [...history, { squares }],
+      history: [...history, { squares, prevMove: i }],
       stepNumber: history.length,
-      xIsNext: !xIsNext
+      xIsNext: !xIsNext,
+      isActive: false
     });
   }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: step % 2 === 0
+      xIsNext: step % 2 === 1,
+      isActive: true
     });
   }
 
+  handleChange() {
+    this.setState({ checked: !this.state.checked });
+  }
+
   render() {
-    const { history, stepNumber } = this.state;
+    const { history, stepNumber, isActive, checked } = this.state;
     const current = history[stepNumber];
     const winner = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
+    let moves = history.map((step, move) => {
+      const desc = move
+        ? "Go to move #" +
+          move +
+          ` in coords. (${indexToRowCol(step.prevMove, 3)})`
+        : "Go to game start";
       return (
         <li key={move}>
-          <button className="btn-history" onClick={() => this.jumpTo(move)}>
+          <button
+            className={`btn-history ${
+              isActive && stepNumber === move ? "active" : ""
+            }`}
+            onClick={() => this.jumpTo(move)}
+          >
             {desc}
           </button>
         </li>
       );
     });
 
+    moves = checked ? moves.reverse() : moves;
     let status;
     if (winner) {
       status = "Winner: " + winner;
+    } else if (isDraw(current.squares)) {
+      status = "Draw!!!";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -63,10 +90,32 @@ export class Game extends Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={i => this.handleclick(i)} />
+          <Board
+            squares={current.squares}
+            isWinner={calculateWinnerIndex(current.squares)}
+            onClick={i => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <label htmlFor="material-switch">
+            <Switch
+              checked={this.state.checked}
+              onChange={() => this.handleChange()}
+              onColor="#86d3ff"
+              onHandleColor="#2693e6"
+              handleDiameter={30}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+              activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+              height={20}
+              width={48}
+              className="react-switch"
+              id="material-switch"
+            />
+            <span className="game-reverse"> Reverse </span>
+          </label>
           <ol>{moves}</ol>
         </div>
       </div>
