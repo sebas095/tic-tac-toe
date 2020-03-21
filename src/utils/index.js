768bc4af1157d406f45cb2f60a752150b3de356e
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-03-20 22:44:53 -0500

    [ADD] utils for game draw added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,15 @@
-export function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
+export function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -17,4 +17,26 @@ export function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+export function calculateWinnerIndex(squares) {
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [a, b, c];
+    }
+  }
+  return [null];
+}
+
+export function isDraw(squares) {
+  const hasWinner = !!calculateWinner(squares);
+  return !hasWinner && !squares.includes(null);
+}
+
+export function indexToRowCol(index, width) {
+  const row = Math.floor(index / width);
+  const col = index % width;
+
+  return [row, col];
 }
